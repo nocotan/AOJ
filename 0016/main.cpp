f78ecf4dd2e71ee67ecc54028ef84c7bfd8bda0a
nocotan <noconoco.lib@gmail.com>
2016-02-14 23:47:34 +0900

    first

--- 0016/main.cpp
+++ 0016/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
     double x = 0;
     double y = 0;
+    double const pi = acos(-1.0);
     int theta = 90;
     return 0;
 }
