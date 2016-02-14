a7f32c232c9a3c19d91546062b5fc4e9c3128240
nocotan <noconoco.lib@gmail.com>
2016-02-14 23:52:13 +0900

    first

--- 0016/main.cpp
+++ 0016/main.cpp
@@ -7,5 +7,19 @@ int main() {
     double y = 0;
     double const pi = acos(-1.0);
     int theta = 90;
+    int d, t;
+    char c;
+
+    for(cin >> d >> c >> t; d != 0 || t != 0; cin >> d >> c >> t) {
+        x += d * cos(pi * theta / 180);
+        y += d * sin(pi * theta / 180);
+        if(theta < 0) {
+            theta += 360;
+        } else {
+            theta %= 360;
+        }
+    }
+    cout << x << endl;
+    cout << y << endl;
     return 0;
 }
