54796cb3200a709f10520cea74ade395c6a7ea87
nocotan <noconoco.lib@gmail.com>
2016-07-11 16:22:35 +0900

    ITP

---
+++ C++/ITP/ITP1_4_D.cpp
@@ -0,0 +1,71 @@
+//#define _GRIBCXX_DEBUG
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+// 基本テンプレート
+#define rep(i,n) for(int i=0; i<(int)n; ++i)
+#define REP(i,x,n) for(int i=x; i<(int)n; ++i)
+#define FOR(i,c) for(__typeof((c).begin())!=(c).begin(); i!=(c).end(); ++i)
+#define ALL(c) (c).begin(), c.end()
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<double> vd;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<long, long> pil;
+
+
+// グラフテンプレート
+typedef int Weight;
+
+struct Edge {
+  int src, dst;
+  Weight weight;
+  Edge(int src, int dst, Weight weight) :
+    src(src), dst(dst), weight(weight) {}
+};
+
+bool operator < (const Edge &e, const Edge &f) {
+  return e.weight != f.weight ? e.weight > f.weight :
+    e.src != f.src ? e.src < f.src : e.dst < f.dst;
+}
+
+typedef vector<Edge> Edges;
+typedef vector<Edges> Graph;
+typedef vector<Weight> Array;
+typedef vector<Array> Matrix;
+
+
+int main() {
+  long long n;
+  long long x, sum=0, mx=-1000000, mn=1000000;
+  scanf("%lld", &n);
+  while(n--) {
+    scanf("%lld", &x);
+    mx = max(mx, x);
+    mn = min(mn, x);
+    sum += x;
+  }
+  printf("%lld %lld %lld\n", mn, mx, sum);
+}
+
---
+++ C++/ITP/ITP1_5_B.cpp
@@ -0,0 +1,79 @@
+//#define _GRIBCXX_DEBUG
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+// 基本テンプレート
+#define rep(i,n) for(int i=0; i<(int)n; ++i)
+#define REP(i,x,n) for(int i=x; i<(int)n; ++i)
+#define FOR(i,c) for(__typeof((c).begin())!=(c).begin(); i!=(c).end(); ++i)
+#define ALL(c) (c).begin(), c.end()
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<double> vd;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<long, long> pil;
+
+
+// グラフテンプレート
+typedef int Weight;
+
+struct Edge {
+  int src, dst;
+  Weight weight;
+  Edge(int src, int dst, Weight weight) :
+    src(src), dst(dst), weight(weight) {}
+};
+
+bool operator < (const Edge &e, const Edge &f) {
+  return e.weight != f.weight ? e.weight > f.weight :
+    e.src != f.src ? e.src < f.src : e.dst < f.dst;
+}
+
+typedef vector<Edge> Edges;
+typedef vector<Edges> Graph;
+typedef vector<Weight> Array;
+typedef vector<Array> Matrix;
+
+
+int main() {
+  int H, W;
+  while(true) {
+    int cnt=1;
+    scanf("%d %d", &H, &W);
+    if(W==0 && H==0) break;
+    rep(i, H) {
+      if(cnt==1||cnt==H) rep(j, W) printf("#");
+      else {
+        printf("#");
+        rep(j, W-2) printf(".");
+        printf("#");
+      }
+      printf("\n");
+      cnt++;
+    }
+    printf("\n");
+  }
+  return 0;
+}
+
