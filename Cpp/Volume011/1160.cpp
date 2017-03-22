2346134b1fa5ceaf96ba0876c41e9561cda34a2f
nocotan <noconoco.lib@gmail.com>
2017-03-22 22:53:21 +0900

    ICPC

---
+++ Cpp/Volume011/1160.cpp
@@ -0,0 +1,96 @@
+//#define _GRIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+// 基本テンプレート
+#pragma region MACRO
+#define P(x) cout << (x) << endl
+#define p(x) cout << (x)
+#define PED cout << "\n"
+#define rep(i,n) for(int i=0; i<(int)n; ++i)
+#define REP(i,x,n) for(int i=x; i<(int)n; ++i)
+#define repi(i,n) for(int i=0; i<=(int)n; ++i)
+#define REPI(i,x,n) for(int i=x; i<=(int)n; ++i)
+#define ILP while(true)
+#define FOR(i,c) for(__typeof((c).begin())!=(c).begin(); i!=(c).end(); ++i)
+#define ALL(c) (c).begin(), (c).end()
+#define mp make_pair
+#pragma endregion
+
+// 型
+#pragma region TYPE_DEF
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<string, string> pss;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<long double> vld;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+#pragma endregion
+
+// Effective std
+#pragma region ESTD
+template<typename C, typename T> int count(C& c, T t) { return count(ALL(c), t); }
+template<typename C, typename F> int count_if(C& c, F f) { return count_if(ALL(c), f); }
+template<typename C, typename T> void erase(C& c, T t) { remove(ALL(c), t), c.end(); }
+template<typename C> void remove(vector<C>& c, unsigned int index) { c.erase(c.begin()+index); }
+template<typename C, typename T, typename U> void replace(C& c, T t, U u) { replace(ALL(c), t, u); }
+template<typename C, typename F, typename U> void replace_if(C& c, F f, U u) { (ALL(c), f, u); }
+template<typename C> void reverse(C& c) { reverse(ALL(c)); }
+template<typename C> void sort(C& c) { sort(ALL(c)); }
+template<typename C, typename Pred> void sort(C& c, Pred p) { sort(ALL(c), p); }
+#pragma endregion
+
+// 定数
+#pragma region CONST_VAL
+constexpr int PI = (2*acos(0.0));
+constexpr int EPS = (1e-9);
+constexpr int MOD = (int)(1e9+7);
+constexpr int INF = 100000000;
+#pragma endregion
+
+int w, h;
+int a[51][51];
+int dx[]{1,-1,0,0,1,-1,1,-1};
+int dy[]{0,0,1,-1,1,-1,-1,1};
+
+void dfs(int x, int y) {
+    a[x][y] = 0;
+    rep(i, 8) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(nx>=0&&nx<h&&ny>=0&&ny<w&&a[nx][ny]==1) {
+            dfs(nx, ny);
+        }
+    }
+}
+
+int main()
+{
+    while(cin>>w>>h, w) {
+        rep(i,h) rep(j,w) a[i][j] = 0;
+
+        rep(i,h) {
+            rep(j,w) {
+                cin >> a[i][j];
+            }
+        }
+
+        int ans = 0;
+        rep(i,h) {
+            rep(j, w) {
+                if(a[i][j]==1) {
+                    dfs(i,j);
+                    ans++;
+                }
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
+
