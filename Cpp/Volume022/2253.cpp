614d973b86510a77b293103c1848f9497b6a34ba
nocotan <noconoco.lib@gmail.com>
2017-03-30 15:26:03 +0900

    ICPC

---
+++ Cpp/Volume022/2253.cpp
@@ -0,0 +1,101 @@
+//#define _GRIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+// 基本テンプレート
+#pragma region MACRO
+#define P(x) cout << (x) << endl
+#define p(x) cout << (x)
+#define PED cout << "\n"
+#define rep(i,n) for(int i=0; i<(int)n; ++i)
+#define REP(i,x,n) for(int i=x; i<(int)n; ++i)
+#define repi(i,n) for(int i=0; i<=(int)n; ++i)
+#define REPI(i,x,n) for(int i=x; i<=(int)n; ++i)
+#define ILP while(true)
+#define FOR(i,c) for(__typeof((c).begin())!=(c).begin(); i!=(c).end(); ++i)
+#define ALL(c) (c).begin(), (c).end()
+#define mp make_pair
+#pragma endregion
+
+// 型
+#pragma region TYPE_DEF
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<string, string> pss;
+typedef pair<string, int> psi;
+typedef pair<int, string> pis;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<long double> vld;
+typedef vector<long> vl;
+typedef vector<long long> vll;
+typedef vector<string> vs;
+#pragma endregion
+
+// Effective std
+#pragma region ESTD
+template<typename C, typename T> int count(C& c, T t) { return count(ALL(c), t); }
+template<typename C, typename F> int count_if(C& c, F f) { return count_if(ALL(c), f); }
+template<typename C, typename T> void erase(C& c, T t) { remove(ALL(c), t), c.end(); }
+template<typename C> void remove(vector<C>& c, unsigned int index) { c.erase(c.begin()+index); }
+template<typename C, typename T, typename U> void replace(C& c, T t, U u) { replace(ALL(c), t, u); }
+template<typename C, typename F, typename U> void replace_if(C& c, F f, U u) { (ALL(c), f, u); }
+template<typename C> void reverse(C& c) { reverse(ALL(c)); }
+template<typename C> void sort(C& c) { sort(ALL(c)); }
+template<typename C, typename Pred> void sort(C& c, Pred p) { sort(ALL(c), p); }
+#pragma endregion
+
+// 定数
+#pragma region CONST_VAL
+constexpr int PI = (2*acos(0.0));
+constexpr int EPS = (1e-9);
+constexpr int MOD = (int)(1e9+7);
+constexpr int INF = 100000000;
+#pragma endregion
+
+#define int long long
+
+const int MAX = 100;
+char flag[MAX*2][MAX*2];
+
+signed main()
+{
+    while(true) {
+        int t, n;
+        cin >> t >> n;
+        if(t==0&&n==0) break;
+        fill(flag[0], flag[MAX*2-1]+MAX*2, 0);
+        for(int i=0; i<n; ++i) {
+            int x, y;
+            cin >> x >> y;
+            flag[y+MAX][x+MAX] = 1;
+        }
+        int sx, sy;
+        cin >> sx >> sy;
+
+        queue<pair<pair<int,int>, int> > q;
+        q.push(make_pair(pair<int, int>(sx,sy),0));
+        int c = 0;
+        while(!q.empty()) {
+            pair<pair<int, int>, int> p = q.front();
+            pair<int, int> a = p.first;
+            int step = p.second;
+            q.pop();
+
+            if(flag[a.second+MAX][a.first+MAX]) continue;
+            flag[a.second+MAX][a.first+MAX] = 1;
+            c++;
+            if(step<t) {
+                q.push(make_pair(pair<int,int>(a.first+1,a.second),step+1));
+                q.push(make_pair(pair<int,int>(a.first-1,a.second),step+1));
+                q.push(make_pair(pair<int,int>(a.first,a.second+1),step+1));
+                q.push(make_pair(pair<int,int>(a.first,a.second-1),step+1));
+                q.push(make_pair(pair<int,int>(a.first+1,a.second+1),step+1));
+                q.push(make_pair(pair<int,int>(a.first-1,a.second-1),step+1));
+            }
+        }
+        cout << c << endl;
+    }
+    return 0;
+}
+
